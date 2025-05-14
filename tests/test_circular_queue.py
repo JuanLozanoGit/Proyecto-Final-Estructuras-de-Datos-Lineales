5781aeda5b67d3c59f4787f9a261724087d01bbb
JuanLozanoGit <juancamilolozanocortesunal@gmail.com>
2025-05-14 09:16:16 -0500

    Update test_circular_queue.py

--- tests/test_circular_queue.py
+++ tests/test_circular_queue.py
@@ -1,1 +1,39 @@
+import unittest
+from src.colas.circular_queue import CircularQueue
+
+class TestCircularQueue(unittest.TestCase):
+
+    def setUp(self):
+        self.queue = CircularQueue(capacity=2)
+
+    def test_enqueue_and_traverse(self):
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.queue.enqueue(3)  # Debe hacer resize
+        self.assertEqual(self.queue.traverse(), [1, 2, 3])
+
+    def test_dequeue_success(self):
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        dequeued = self.queue.dequeue()
+        self.assertEqual(dequeued, 1)
+        self.assertEqual(self.queue.traverse(), [2])
+
+    def test_dequeue_empty(self):
+        dequeued = self.queue.dequeue()
+        self.assertIsNone(dequeued)
+
+    def test_peek(self):
+        self.queue.enqueue(10)
+        self.assertEqual(self.queue.peek(), 10)
+        self.queue.dequeue()
+        self.assertIsNone(self.queue.peek())
+
+    def test_get_size(self):
+        self.assertEqual(self.queue.get_size(), 0)
+        self.queue.enqueue(1)
+        self.assertEqual(self.queue.get_size(), 1)
+
+if __name__ == '__main__':
+    unittest.main()
 
