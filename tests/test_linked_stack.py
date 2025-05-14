df3063c10f72a8dd6ec33ffa55a31b98f12dd9f4
JuanLozanoGit <juancamilolozanocortesunal@gmail.com>
2025-05-14 09:15:17 -0500

    Update test_linked_stack.py

--- tests/test_linked_stack.py
+++ tests/test_linked_stack.py
@@ -1,1 +1,39 @@
+import unittest
+from src.pilas.linked_stack import LinkedStack
+
+class TestLinkedStack(unittest.TestCase):
+
+    def setUp(self):
+        self.stack = LinkedStack()
+
+    def test_push_and_traverse(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+        self.assertEqual(self.stack.traverse(), [3, 2, 1])
+
+    def test_pop_success(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        popped = self.stack.pop()
+        self.assertEqual(popped, 2)
+        self.assertEqual(self.stack.traverse(), [1])
+
+    def test_pop_empty(self):
+        popped = self.stack.pop()
+        self.assertIsNone(popped)
+
+    def test_peek(self):
+        self.stack.push(10)
+        self.assertEqual(self.stack.peek(), 10)
+        self.stack.pop()
+        self.assertIsNone(self.stack.peek())
+
+    def test_get_size(self):
+        self.assertEqual(self.stack.get_size(), 0)
+        self.stack.push(1)
+        self.assertEqual(self.stack.get_size(), 1)
+
+if __name__ == '__main__':
+    unittest.main()
 
